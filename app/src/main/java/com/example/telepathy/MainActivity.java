04cd04c4e0e846592ab59980958f4e6790da273f
Nibba2018 <sohambiswas41@gmail.com>
2020-04-20 20:41:09 +0530

    added on click callbacks

--- app/src/main/java/com/example/telepathy/MainActivity.java
+++ app/src/main/java/com/example/telepathy/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.telepathy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
             this.getSupportActionBar().hide();
         }catch (NullPointerException e){}
 
+        ImageButton comp_guess = (ImageButton) findViewById(R.id.comp_guess);
+        ImageButton guess_comp = (ImageButton) findViewById(R.id.guess_comp);
 
+        comp_guess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        guess_comp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
