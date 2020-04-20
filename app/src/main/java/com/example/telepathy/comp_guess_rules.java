99cfde63025b93cadb1cfb0a07082a5c6c9c99d0
Nibba2018 <sohambiswas41@gmail.com>
2020-04-20 22:06:47 +0530

    Added comp_guess_rules

--- app/src/main/java/com/example/telepathy/comp_guess_rules.java
+++ app/src/main/java/com/example/telepathy/comp_guess_rules.java
@@ -7,34 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class comp_guess_rules extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_comp_guess_rules);
 
         try {
             this.getSupportActionBar().hide();
         } catch (NullPointerException e) {
         }
 
-        ImageButton comp_guess = (ImageButton) findViewById(R.id.comp_guess);
-        ImageButton guess_comp = (ImageButton) findViewById(R.id.guess_comp);
+        ImageButton continueButton = (ImageButton) findViewById(R.id.continueButton);
 
-        comp_guess.setOnClickListener(new View.OnClickListener() {
+        continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goToRules = new Intent();
-                goToRules.setClass(getApplicationContext(), comp_guess_rules.class);
+                goToRules.setClass(getApplicationContext(), .class);
                 startActivity(goToRules);
-            }
-        });
-
-        guess_comp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
             }
         });
     }
