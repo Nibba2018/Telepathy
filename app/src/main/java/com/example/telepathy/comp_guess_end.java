89d63a33517dc7beab0a306727600576fac07f3b
Nibba2018 <sohambiswas41@gmail.com>
2020-04-22 00:50:19 +0530

    Completed comp guess game

--- app/src/main/java/com/example/telepathy/comp_guess_end.java
+++ app/src/main/java/com/example/telepathy/comp_guess_end.java
@@ -7,6 +7,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class comp_guess_end extends AppCompatActivity implements View.OnClickListener{
@@ -15,6 +16,12 @@ public class comp_guess_end extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comp_guess_end);
+
+        Intent guess = getIntent();
+
+        TextView result = (TextView) findViewById(R.id.comp_guess_result);
+        String query = String.valueOf(result.getText()) + guess.getStringExtra("Val");
+        result.setText(query);
 
         ImageButton replayButton = (ImageButton) findViewById(R.id.replay);
         ImageButton quitButton = (ImageButton) findViewById(R.id.quitButton);
--- app/src/main/java/com/example/telepathy/comp_guess_game.java
+++ app/src/main/java/com/example/telepathy/comp_guess_game.java
@@ -7,6 +7,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class comp_guess_game extends AppCompatActivity implements View.OnClickListener{
@@ -20,8 +21,10 @@ public class comp_guess_game extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comp_guess_game);
 
+        TextView mid_val = (TextView) findViewById(R.id.mid_result);
+
         this.mid = (this.lower_bound + this.upper_bound)/2;
-        Toast.makeText(getApplicationContext(), "Is your number:" + String.valueOf(this.mid) + "?", Toast.LENGTH_SHORT).show();
+        mid_val.setText(String.valueOf(this.mid)+"?");
 
         ImageButton goLower = (ImageButton) findViewById(R.id.go_lower);
         ImageButton goHigher = (ImageButton) findViewById(R.id.go_higher);
@@ -34,17 +37,18 @@ public class comp_guess_game extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v){
+        TextView mid_val = (TextView) findViewById(R.id.mid_result);
         switch (v.getId()){
             case R.id.go_lower: this.upper_bound = this.mid - 1; break;
             case R.id.go_higher: this.lower_bound = this.mid + 1; break;
             case R.id.correctButton:{
                 Intent goToEnd = new Intent();
                 goToEnd.setClass(this, comp_guess_end.class);
+                goToEnd.putExtra("Val", String.valueOf(this.mid) + '!');
                 startActivity(goToEnd);
             }
         }
-
-        Toast.makeText(getApplicationContext(), "Is your number:" + String.valueOf(this.mid) + "?", Toast.LENGTH_SHORT).show();
         this.mid = (this.upper_bound + this.lower_bound)/2;
+        mid_val.setText(String.valueOf(this.mid)+"?");
     }
 }
